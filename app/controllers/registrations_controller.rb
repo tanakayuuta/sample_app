1cfa085668827bc1b1a3f50c01a11181796991c1
Yuuta Tanaka <tanakayuuta0321@gmail.com>
2017-10-02 13:47:16 +0000

    RegistrationsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,16 @@
-class RelationshipsController < ApplicationController
-  before_action :authenticate_user!
+class RegistrationsController < Devise::RegistrationsController
 
-  def create
-    @user = User.find(params[:relationship][:followed_id])
-    current_user.follow!(@user)
-    redirect_to @user
-  end
-
-  def destroy
-    @user = Relationship.find(params[:id]).followed
-    current_user.unfollow!(@user)
-    redirect_to @user
-  end
+ def after_sign_up_path_for(resource)
+     user_path(resource)
+ end
+ 
+ private
+  
+       def sign_up_params
+         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+       end
+  
+       def account_update_params
+ 　        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+       end
 end
