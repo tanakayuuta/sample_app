3468b15e4eb0831d5e0cd58f5f2ceae1f2f65abc
Yuuta Tanaka <tanakayuuta0321@gmail.com>
2017-11-07 12:41:30 +0000

    s3setteisyuusei

--- Gemfile
+++ Gemfile
@@ -8,9 +8,10 @@ gem 'sprockets', '2.11.0'
 gem 'faker', '1.1.2'
 gem 'will_paginate', '3.0.4'
 gem 'will_paginate-bootstrap', '1.0.1'
-gem "paperclip"
-
-gem 'aws-sdk', '~> 2.3' # AWS
+#gem "paperclip"
+#gem 'aws-sdk', '~> 2.3' # AWS
+gem 'paperclip', '~> 4.2.1'
+gem 'aws-sdk', '< 2.0'
 
 group :development do
   gem 'sqlite3', '1.3.9'
@@ -26,4 +27,9 @@ gem 'jbuilder', '1.5.3'
 
 group :doc do
   gem 'sdoc', '0.4.1', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,15 @@ SampleApp::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+   
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_host_name: "s3-ap-northeast-1.amazonaws.com",
+    }
+  }
 end
